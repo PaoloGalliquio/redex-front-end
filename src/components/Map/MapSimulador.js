fa590ee23c9f169119b09d15d64bb24d26b923e1
MiguelLaTorre01 <mlatorreb@pucp.edu.pe>
2022-10-18 14:38:28 -0500

    'vueloAnimado'

--- src/components/Map/MapSimulador.js
+++ src/components/Map/MapSimulador.js
@@ -6,6 +6,8 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import GeoJSONTerminator from "@webgeodatavore/geojson.terminator";
 import Legend from "../Legend/Legend";
 import * as turf from "@turf/turf";
+import "./MapSimulador.css";
+import myImage from "../../images/torre.png";
 
 mapboxgl.workerClass = MapboxWorker;
 
@@ -14,9 +16,9 @@ const MapSimulador = (props) => {
   const [lng, setLng] = useState(0);
   const [lat, setLat] = useState(0);
   const [zoom, setZoom] = useState(1);
-  const [aeropuertos2, setAeropuertos] = useState([]);
   const mapContainer = useRef(null);
   const map = useRef(null);
+  var geoJSON = new GeoJSONTerminator();
 
   const aeropuertos = [
     {
@@ -261,25 +263,29 @@ const MapSimulador = (props) => {
       }
   ];
 
+  let nroVuelos=0;
+  let vueloListo=1;
+  const vuelos = [];
+
   const origin = [-122.414, 37.776];
   const destination = [-77.032, 38.913];
 
   //declarar la línea para recorrer
-  const route = {
+  let route = {
     type: "FeatureCollection",
     features: [
       {
         type: "Feature",
         geometry: {
           type: "LineString",
-          coordinates: [origin, destination],
-        },
-      },
-    ],
+          coordinates: [origin, destination]
+        }
+      }
+    ]
   };
 
   //el punto que estará en animación, comienza en 'origin'
-  const point = {
+  let point = {
     type: "FeatureCollection",
     features: [
       {
@@ -287,44 +293,39 @@ const MapSimulador = (props) => {
         properties: {},
         geometry: {
           type: "Point",
-          coordinates: origin,
-        },
-      },
-    ],
+          coordinates: origin
+        }
+      }
+    ]
   };
 
-  const lineDistance = turf.length(route.features[0]);
-  const arc = [];
   const steps = 500;
-
-  for (let i = 0; i < lineDistance; i += lineDistance / steps) {
-    const segment = turf.along(route.features[0], i);
-    arc.push(segment.geometry.coordinates);
-  }
-
-  route.features[0].geometry.coordinates = arc;
   let counter = 0;
 
   function animate() {
     const start =
-      route.features[0].geometry.coordinates[
+      route.features[nroVuelos-1].geometry.coordinates[
         counter >= steps ? counter - 1 : counter
       ];
     const end =
-      route.features[0].geometry.coordinates[
+      route.features[nroVuelos-1].geometry.coordinates[
         counter >= steps ? counter : counter + 1
       ];
-    if (!start || !end) return;
+    if (!start || !end) {
+      counter=0;
+      return;
+    }
 
-    point.features[0].geometry.coordinates =
-      route.features[0].geometry.coordinates[counter];
+    point.features[nroVuelos-1].geometry.coordinates =
+      route.features[nroVuelos-1].geometry.coordinates[counter];
 
-    point.features[0].properties.bearing = turf.bearing(
+    point.features[nroVuelos-1].properties.bearing = turf.bearing(
       turf.point(start),
       turf.point(end)
     );
 
     map.current.getSource("point").setData(point);
+    map.current.getSource("route").setData(route);
 
     if (counter < steps) {
       requestAnimationFrame(animate);
@@ -333,16 +334,123 @@ const MapSimulador = (props) => {
     counter = counter + 1;
   }
 
-  const getAeropuertos = () => {
-        /*axios.get(
-            USUARIO_API_BASE_URL + 'especialidad/list')
-            .then(response => {
-                setAeropuertos(response.data);
-            })
-            .catch(error => {
-                values.mensaje = 'Fallo al listar aeropuertos';
-            })*/
+  const trazarRutas = (index) => {
+    const lineDistance = turf.length(route.features[index]);
+    let arc = [];
+    const steps = 100;
+
+    for (let i = 0; i < lineDistance; i += lineDistance / steps) {
+      const segment = turf.along(route.features[index], i);
+      arc.push(segment.geometry.coordinates);
     }
+
+    route.features[index].geometry.coordinates = arc;
+    //let counter = 0;
+
+    setTimeout(() => {
+      //animate(counter, index);
+      //animacion(counter, index);
+      animate(counter);
+    }, 1000);
+  }
+
+  const getAeropuertos = () => {
+    aeropuertos.forEach(element => {
+        let description = `<b>${element.codigo}</b> (GMT-)<br>Capacidad: 100`;
+        const el = document.createElement('div');
+        el.className = 'marker';
+        el.style.backgroundImage = `url(${myImage})`;
+        el.style.filter = 'invert(19%) sepia(19%) saturate(3817%) hue-rotate(85deg) brightness(96%) contrast(88%)';
+        el.style.width = `25px`;
+        el.style.height = `25px`;
+        el.style.backgroundSize = '100%';
+
+        //al posar el cursor sobre el ícono de aeropuerto..
+        el.addEventListener('mouseenter', () => {
+          popup.setLngLat([element.lng, element.lat]).setHTML(description).addTo(map.current);
+        });
+
+        //al retirar el cursor sobre el ícono de aeropuerto...
+        el.addEventListener('mouseleave', () => {
+          popup.remove();
+          //actualizar descripcion del aeropuerto
+          //description = 'cambio a <b>'+Math.random()+'</b>';
+          //actualizar color del aeropuerto de acuerdo a la capacidad
+          //el.style.filter = 'invert(21%) sepia(79%) saturate(6123%) hue-rotate(355deg) brightness(92%) contrast(116%)';
+        });
+
+        //al clickear sobre el ícono de aeropuerto...
+        //adicionalment, se podría configurar algún evento que se active cuando se actualicen cantidades
+        el.addEventListener('click', () => {
+
+          if(vueloListo == 1){
+            //selecciona punto de origen
+            //agregar vuelo
+            vuelos.push({
+              id: nroVuelos+1,
+              idPartida: element.id-1,
+              idDestino: 0
+            });
+
+            //en la espera del punto de destino
+            vueloListo++;
+          }else if(vueloListo == 2){
+            let index = nroVuelos;
+            //selecciona punto de destino
+            vuelos[nroVuelos].idDestino = element.id-1;
+            
+            //se reinicia la cuenta
+            nroVuelos++;
+            vueloListo = 1;
+            setTimeout(() => {
+              //console.log(aeropuertos[vuelos[nroVuelos-1].idPartida].codigo+' -> '
+              //+aeropuertos[vuelos[nroVuelos-1].idDestino].codigo);
+              if(nroVuelos>1){
+                route.features.push({
+                  type: "Feature",
+                  geometry: {
+                    type: "LineString",
+                    coordinates: [[aeropuertos[vuelos[nroVuelos-1].idPartida].lng,aeropuertos[vuelos[nroVuelos-1].idPartida].lat],[aeropuertos[vuelos[nroVuelos-1].idDestino].lng,aeropuertos[vuelos[nroVuelos-1].idDestino].lat]]
+                  }
+                });
+
+                point.features.push({
+                  type: "Feature",
+                  properties: {},
+                  geometry: {
+                    type: "Point",
+                    coordinates: [aeropuertos[vuelos[nroVuelos-1].idPartida].lng,aeropuertos[vuelos[nroVuelos-1].idPartida].lat]
+                  }
+                });
+
+
+              }else{
+                //1er vuelo
+                route.features[0].geometry.coordinates = [[aeropuertos[vuelos[nroVuelos-1].idPartida].lng,aeropuertos[vuelos[nroVuelos-1].idPartida].lat],[aeropuertos[vuelos[nroVuelos-1].idDestino].lng,aeropuertos[vuelos[nroVuelos-1].idDestino].lat]];
+                point.features[0].geometry.coordinates = [aeropuertos[vuelos[nroVuelos-1].idPartida].lng,aeropuertos[vuelos[nroVuelos-1].idPartida].lat];
+              }
+              trazarRutas(index);
+            }, 1000);
+          }
+        });
+
+        const popup = new mapboxgl.Popup({
+          closeButton: false,
+          closeOnClick: false
+        });
+        
+        // Add markers to the map.
+        new mapboxgl.Marker(el)
+        .setLngLat([element.lng, element.lat])
+        .addTo(map.current);
+
+        //#124d12: verde, #f6fa02: amarillo, #fa0202:rojo
+        //verde: invert(19%) sepia(19%) saturate(3817%) hue-rotate(85deg) brightness(96%) contrast(88%)
+        //amarillo: invert(82%) sepia(63%) saturate(882%) hue-rotate(11deg) brightness(113%) contrast(107%)
+        //rojo: invert(21%) sepia(79%) saturate(6123%) hue-rotate(355deg) brightness(92%) contrast(116%)
+        //codeColor to filter: https://codepen.io/sosuke/pen/Pjoqqp
+    })
+  }
 
   useEffect(() => {
     if (map.current) return;
@@ -351,64 +459,62 @@ const MapSimulador = (props) => {
       style: "mapbox://styles/mapbox/streets-v11",
       center: [lng, lat],
       zoom: zoom,
-      dragRotate: false,
+      dragRotate: false
     });
 
-    getAeropuertos();
+    //evitar copias horizontales del mundo
+    map.current.setRenderWorldCopies(false);
 
     setTimeout(() => {
+      getAeropuertos();
 
-        aeropuertos.forEach(element => {
-            new mapboxgl.Marker({ color: '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6) })
-            .setLngLat([element.lng, element.lat])
-            .addTo(map.current);
-        })
-
-        new mapboxgl.Marker({ color: 'grey' })
-        .setLngLat(origin)
-        .addTo(map.current);
-
-        new mapboxgl.Marker({ color: 'blue' })
-        .setLngLat(destination)
-        .addTo(map.current);
-
-        map.current.addSource('route', {
+      map.current.addLayer({
+        'id': 'daynight',
+        'type': 'fill',
+        'source': {
             'type': 'geojson',
-            'data': route
-            });
-            
-        map.current.addSource('point', {
-            'type': 'geojson',
-            'data': point
-        });
-            
-        map.current.addLayer({
-            'id': 'route',
-            'source': 'route',
-            'type': 'line',
-            'paint': {
-                'line-width': 2,
-                'line-color': '#007cbf'
-            }
-        });
-            
-        map.current.addLayer({
-            'id': 'point',
-            'source': 'point',
-            'type': 'symbol',
-            'layout': {
-                'icon-image': 'airport-15',
-                'icon-size': 2,
-                'icon-rotate': ['get', 'bearing'],
-                'icon-rotation-alignment': 'map',
-                'icon-allow-overlap': true,
-                'icon-ignore-placement': true
-            }
-        });
+            'data': geoJSON
+        },
+        'layout': {},
+        'paint': {
+            'fill-color': '#000',
+            'fill-opacity': 0.2
+        }
+      });
 
-        setTimeout(() => {
-            animate(counter);
-        }, 2000);
+      map.current.addSource('route', {
+          'type': 'geojson',
+          'data': route
+          });
+          
+      map.current.addSource('point', {
+          'type': 'geojson',
+          'data': point
+      });
+          
+      map.current.addLayer({
+          'id': 'route',
+          'source': 'route',
+          'type': 'line',
+          'paint': {
+              'line-width': 2,
+              'line-color': '#007cbf'
+          }
+      });
+          
+      map.current.addLayer({
+          'id': 'point',
+          'source': 'point',
+          'type': 'symbol',
+          'layout': {
+              'icon-image': 'airport-15',
+              'icon-size': 2,
+              'icon-rotate': ['get', 'bearing'],
+              'icon-rotation-alignment': 'map',
+              'icon-allow-overlap': true,
+              'icon-ignore-placement': true
+          }
+      });
     }, 2000);
 
     
