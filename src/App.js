23d8190b82dbc3816408ba9cb0458b6b547c47b2
PaoloGalliquio <paolo.galliquio@bit2bitamericas.com>
2022-12-03 00:29:02 -0500

    asda

--- src/App.js
+++ src/App.js
@@ -4,25 +4,29 @@ import OperacionesDiarias from "./pages/OperacionesDiarias/OperacionesDiarias";
 import Simulador from "./pages/Simulador/Simulador";
 import Configuracion from "./pages/Configuracion/Configuracion";
 import NuevoEnvio from "./pages/NuevoEnvio/NuevoEnvio";
-import ChatRoom from './components/ChatRoom'
+import ChatRoom from './components/ChatRoom';
 import React from 'react';
 
+// function App() {
+//   return (
+//     <div className="App contenedor">
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<OperacionesDiarias />} />
+//           <Route path="OperacionesDiarias" element={<OperacionesDiarias />} />
+//           <Route path="Simulador" element={<Simulador />} />
+//           <Route path="ColapsoLogistico" element={<OperacionesDiarias />} />
+//           <Route path="Configuracion" element={<Configuracion />} />
+//           <Route path="NuevoEnvio" element={<NuevoEnvio />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// Chat de web socket
 function App() {
-  return (
-    {/*<div className="App contenedor">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<OperacionesDiarias />} />
-          <Route path="OperacionesDiarias" element={<OperacionesDiarias />} />
-          <Route path="Simulador" element={<Simulador />} />
-          <Route path="ColapsoLogistico" element={<OperacionesDiarias />} />
-          <Route path="Configuracion" element={<Configuracion />} />
-          <Route path="NuevoEnvio" element={<NuevoEnvio />} />
-        </Routes>
-      </BrowserRouter>
-    </div> */}
-    <ChatRoom />
-  );
+  return <ChatRoom />;
 }
 
 export default App;
